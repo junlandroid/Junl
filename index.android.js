09b20ab08fd27ce9444f44d0cdc3e158802d7353
junlandroid <15800904094@163.com>
2017-08-18 20:06:10 +0800

    ReactNative之View组件

--- index.android.js
+++ index.android.js
@@ -16,20 +16,14 @@ export default class Junl extends Component {
     render() {
         return (
             <View style={styles.container}>
-                <Text style={styles.welcome}>
-                    Welcome to React Native!
-                </Text>
-                <Text style={styles.instructions}>
-                    To get started, edit index.android.js
-                </Text>
-                <Text style={styles.junl}>
-                      Hello Junl,Welcom ReactNative.
+                <View style={[styles.item,styles.center]}>
+                    <Text>酒店</Text>
+                </View>
+                <View style={styles.item}>
 
-                </Text>
-                <Text style={styles.instructions}>
-                    Double tap R on your keyboard to reload,{'\n'}
-                    Shake or press menu button for dev menu
-                </Text>
+
+                </View>
+                <View style={styles.item}></View>
             </View>
         );
     }
@@ -38,25 +32,22 @@ export default class Junl extends Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#F5FCFF',
+        borderWidth: 1,//边框
+        borderColor: 'red',
+        flexDirection: 'row',//方向
     },
-    welcome: {
-        fontSize: 20,
-        textAlign: 'center',
-        margin: 10,
+    item: {
+        flex: 1,
+        height: 80,
+        borderWidth: 1,
+        borderColor: 'blue'
     },
-    instructions: {
-        textAlign: 'center',
-        color: '#333333',
-        marginBottom: 5,
-    },
-    junl:{
-        fontSize:30,
-        textAlign:'center',
-        color:'red',
+    center:{
+        justifyContent:'center',
+        alignItems:'center',
     }
+
+
 });
 
 AppRegistry.registerComponent('Junl', () => Junl);
